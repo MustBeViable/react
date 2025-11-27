d43815202f21c6fb541db4b5c1b6bfbbdd624477
Elias Rinne <elias1rinne@gmail.com>
2025-11-27 13:24:45 +0200

    lab 1 complete

--- react-app/src/views/Home.jsx
+++ react-app/src/views/Home.jsx
@@ -1,5 +1,4 @@
 import MediaRow from '../components/MediaRow';
-import '../App.css';
 import { useState } from 'react';
 import { useMedia } from '../hooks/apiHooks';
 
@@ -13,34 +12,56 @@ const Home = () => {
 
   return (
     <>
-      <h2>My Media</h2>
-      <table className="pizza-card">
-        <thead>
+      <h2 className="mb-4 text-2xl font-semibold">My Media</h2>
+
+      <table className="w-full border-collapse text-sm">
+        <thead className="bg-neutral-800 text-neutral-100">
           <tr>
-            <th>Thumbnail</th>
-            <th>Title</th>
-            <th>Owner</th>
-            <th>Description</th>
-            <th>Created</th>
-            <th>Size</th>
-            <th>Type</th>
-            <th>View</th>
+            <th className="p-4 border border-neutral-700">Thumbnail</th>
+            <th className="p-4 border border-neutral-700">Title</th>
+            <th className="p-4 border border-neutral-700">Owner</th>
+            <th className="p-4 border border-neutral-700">Description</th>
+            <th className="p-4 border border-neutral-700">Created</th>
+            <th className="p-4 border border-neutral-700">Size</th>
+            <th className="p-4 border border-neutral-700">Type</th>
+            <th className="p-4 border border-neutral-700">View</th>
           </tr>
         </thead>
-        <tbody>
-          {mediaArray.map((item) => {
-            return (
-              <MediaRow
-                key={item.media_id}
-                item={item}
-                className="pizza-card"
-                selectedItem={selectedItem}
-                setSelectedItem={setSelectedItem}
-                isOpen={isOpen}
-                setIsOpen={setIsOpen}
-              />
-            );
-          })}
+
+        <tbody
+          className="
+            [&>tr:nth-child(odd)]:bg-neutral-900
+            [&>tr:nth-child(even)]:bg-neutral-800
+
+            [&>tr>td]:p-4
+            [&>tr>td]:border
+            [&>tr>td]:border-neutral-700
+            [&>tr>td]:align-middle
+            [&>tr>td]:text-center
+
+            [&>tr>td:first-child>img]:w-[260px]
+            [&>tr>td:first-child>img]:h-[200px]
+            [&>tr>td:first-child>img]:object-cover
+
+            [&>tr>td:last-child>a]:inline-block
+            [&>tr>td:last-child>a]:px-2
+            [&>tr>td:last-child>a]:py-1
+            [&>tr>td:last-child>a]:bg-[#363636]
+            [&>tr>td:last-child>a]:text-white
+            [&>tr>td:last-child>a]:no-underline
+            [&>tr>td:last-child>a:hover]:bg-[#111111]
+          "
+        >
+          {mediaArray.map((item) => (
+            <MediaRow
+              key={item.media_id}
+              item={item}
+              selectedItem={selectedItem}
+              setSelectedItem={setSelectedItem}
+              isOpen={isOpen}
+              setIsOpen={setIsOpen}
+            />
+          ))}
         </tbody>
       </table>
     </>
